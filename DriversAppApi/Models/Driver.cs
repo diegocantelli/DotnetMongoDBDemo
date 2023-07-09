fcc34b45994d702dc06a5763f3254f5d6dc14f15
diegocantelli <diegocantelli19@gmail.com>
2023-07-09 10:57:50 -0300

    Criando a model Driver e realizando o mapeamento entre os campos da collection

---
+++ DriversAppApi/Models/Driver.cs
@@ -0,0 +1,16 @@
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DriversAppApi.Models 
+{
+    public class Driver 
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")] // Name sera o nome definido na collection do mongoDB
+        public string DriverName { get; set; }
+
+        public int Number { get; set; }
+    }
+}
