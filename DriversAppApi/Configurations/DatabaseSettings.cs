38053aea5d612c3cfff5efa13c73fcbfe9ea9385
diegocantelli <diegocantelli19@gmail.com>
2023-07-09 12:48:42 -0300

    Lendo as configs do appsettings e carregando dentro da model de configuracao do MongoDB

---
+++ DriversAppApi/Configurations/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+namespace DriversAppApi.Configurations
+{
+    public class DatabaseSettings 
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName  { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
