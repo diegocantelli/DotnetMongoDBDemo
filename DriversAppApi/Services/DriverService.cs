5f1ea02bd5b083b5bdce0e733ce767b51711d808
diegocantelli <diegocantelli19@gmail.com>
2023-07-09 13:08:20 -0300

    Criando a classe de servico responsavel pelas operacoes de CRUD

--- DriversAppApi/Services/DriverService.cs
+++ DriversAppApi/Services/DriverService.cs
@@ -17,5 +17,16 @@ namespace DriversAppApi.Services
         }
 
         public async Task<List<Driver>> GetAsync() => await _driverCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Driver> GetAsync(string id) => await _driverCollection.Find(x => x.Id == id)
+            .FirstOrDefaultAsync(); 
+
+        public async Task CreateAsync(Driver driver) => await _driverCollection.InsertOneAsync(driver);
+
+        public async Task UpdateAsync(Driver driver) => await _driverCollection
+            .ReplaceOneAsync(x => x.Id == driver.Id, driver);
+
+        public async Task DeleteAsync(string id) => await _driverCollection
+            .DeleteOneAsync(x => x.Id == id);
     }
 }
